f62a3ec7c37cbbf2753866bd95df874405433252
jz117 <josezarateb312@gmail.com>
2023-11-07 02:10:00 -0500

    Created ReorientVentricleCopy
    This fxn bases the output volume on the croppedImage size. It iterates through the third dimension and performs the cropping on all slices and then attempts to construct a 4D matrix containing the cropped slices.
    main now calls ReorientVentricleCopy

--- main.m
+++ main.m
@@ -4,12 +4,20 @@ clc;
 clear variables;
 close all;
 
+heart = resampleDicom('05.dcm');
+
 % Call the ReorientVentricle function
-[outputImage, selectedDimension] = ReorientVentricle();
+%[outputVolume, selectedDimension] = ReorientVentricle(heart);
+[outputVolume, selectedDimension] = ReorientVentricleCopy(heart);
+outputImageSize = size(outputVolume);
+disp(outputImageSize);
+
+displayNewSlices(outputImage);
+
 
 if ~isempty(outputImage)
     % Call the drawEndocardialBoundary function to draw the endocardial boundary
-    drawEndocardialBoundary(outputImage, selectedDimension);
+    drawEndocardialBoundary(imageVolume, outputImage, selectedDimension);
 else
     disp('ReorientVentricle function did not return an image.');
 end
