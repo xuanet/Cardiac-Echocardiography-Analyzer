d6376e5d7f087b1e1c0c0a0414006939edaa8420
nidhi-srivaths <150309981+nidhi-srivaths@users.noreply.github.com>
2023-11-24 11:30:10 -0500

    Add files via upload
    What works: 1) averages all areas 2) i had a slider feature (not in this commit) that lets you go slide thru all frames before choosing one but its super slow
    What doesn't work: No clear view of aorta because I couldn't rotate the slice
    Dataset: Working with 002 because its more/less already axis aligned. Didn't want to introduce a new source of error when trying to troubleshoot this.

---
+++ AorticArea.m
@@ -0,0 +1,50 @@
+heart = resampleDicom('002A.dcm'); %(Using a pre-aligned set to troubleshoot aorta area and not realignement)
+centerH = floor(heart.height / 2);
+centerW = floor(heart.width / 2);
+centerD = floor(heart.depth / 2);
+
+numTimeFrames = heart.NumVolumes;
+
+% Create a figure
+fig = figure;
+
+% Initialize total area
+totalArea = 0;
+
+%Select depth slice
+currentDepth=42;
+
+    for frame = 1:numTimeFrames
+        extractD = heart.data(:, :, currentDepth, frame);
+        imgD = squeeze(extractD);
+        imshow(imgD', []);
+        hFH = drawfreehand;
+        
+        % Get the binary mask of the drawn region
+        if exist('hFH', 'var')
+            binaryMask = createMask(hFH);
+        else
+            binaryMask = true(size(imgD));
+        end
+        
+        % Calculate pixel spacing based on provided dimensions
+        pixelSpacingH = heart.heightspan / heart.height_padded;
+        pixelSpacingD = heart.depthspan / heart.depth_padded;
+        pixelSpacingW = heart.widthspan / heart.width_padded;
+        
+        % Get pixel spacing information for specific slice
+        pixelSpacing = [pixelSpacingW, pixelSpacingH];
+        
+        % Calculate the area
+        areaInPixels = sum(binaryMask(:));
+        areaInUnits = areaInPixels * prod(pixelSpacing);
+        
+        disp(['Area of the drawn region in frame ', num2str(frame), ' at depth ', num2str(currentDepth), ': ', num2str(areaInUnits), ' cm2']);
+        
+        % Accumulate the area for the current depth
+        areaForCurrentDepth = areaForCurrentDepth + areaInUnits;
+    end
+    
+    % Calculate and display the average area for the current depth
+    averageAreaForCurrentDepth = areaForCurrentDepth / numTimeFrames;
+    disp(['Average area over all frames at depth slice#', num2str(currentDepth), ': ', num2str(averageAreaForCurrentDepth), ' square cm']);
